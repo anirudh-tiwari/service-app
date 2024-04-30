c3fa9e847892ecc92ef51c0734007df686eb7ef8
anirudh-tiwari <anirudht2001@gmail.com>
2024-04-30 22:10:25 +0530

    add: video component updated

--- pages/health/index.js
+++ pages/health/index.js
@@ -1,27 +1,23 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Video from "./video";
 // import Carousel from '../../components/carousel';
 
 const Health = () => {
   return (
     <View style={styles.healthWrap}>
-    <Image source={require('../../assets/thumbnil.png')} style={{
-      height: 160,
-      width: '100%',
-      objectFit: "cover",
-      borderRadius: 24,
-    }} />
+      <Video />
     </View>
-  )
-}
+  );
+};
 
 export default Health;
 
 const styles = StyleSheet.create({
-    healthWrap : {
-        backgroundColor : 'black',
-        flex: 1,
-        justifyConttent: 'center',
-        paddingVertical: 24,
-        paddingHorizontal: 24,
-    }
-})
+  healthWrap: {
+    backgroundColor: "black",
+    flex: 1,
+    justifyConttent: "center",
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+  },
+});
---
+++ pages/health/video.js
@@ -0,0 +1,49 @@
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native'
+
+const Video = () => {
+  return (
+    <View style={styles.imageWrap}>
+      <Image
+        source={require("../../assets/thumbnil.png")}
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          borderRadius: 24,
+        }}
+      />
+      <TouchableOpacity
+        style={[styles.navigation, { top: "50%", left: "42%" }]}
+      >
+        <Image
+          source={require("../../assets/play-circle.png")}
+          style={{
+            width: 50,
+            height: 50,
+            objectFit: "cover",
+            opacity: 0.4
+          }}
+        />
+      </TouchableOpacity>
+      </View>
+  )
+}
+
+export default Video;
+
+const styles = StyleSheet.create({
+    imageWrap: {
+      height: 160,
+      width: "100%",
+    },
+    navigation: {
+      padding: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+      width: 40,
+      height: 30,
+      position: "absolute",
+      top: 24,
+    },
+  });
