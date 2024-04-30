8b77bc2ac153b6eb485832a697c71b6872e9fc34
anirudh-tiwari <anirudht2001@gmail.com>
2024-04-30 08:20:15 +0530

    add: thumbnail update

--- pages/health/index.js
+++ pages/health/index.js
@@ -1,10 +1,15 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 // import Carousel from '../../components/carousel';
 
 const Health = () => {
   return (
-    <View style={ styles.healthWrap }>
-      {/* <Carousel /> */}
+    <View style={styles.healthWrap}>
+    <Image source={require('../../assets/thumbnil.png')} style={{
+      height: 160,
+      width: '100%',
+      objectFit: "cover",
+      borderRadius: 24,
+    }} />
     </View>
   )
 }
@@ -13,8 +18,10 @@ export default Health;
 
 const styles = StyleSheet.create({
     healthWrap : {
-        backgroundColor : '#EFD8D1',
+        backgroundColor : 'black',
         flex: 1,
         justifyConttent: 'center',
+        paddingVertical: 24,
+        paddingHorizontal: 24,
     }
 })
