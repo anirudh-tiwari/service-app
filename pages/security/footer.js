73e1fff0e2b78af5d0a9d006cda8775f9182983e
anirudh-tiwari <anirudht2001@gmail.com>
2024-05-16 22:04:34 +0530

    add: complete Footer section

--- pages/security/footer.js
+++ pages/security/footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   const [selectedTab, setSelectedTab] = useState(0);
 
   return (
-    <View style={{flex:1}}>
-         {selectedTab == 0 ? (
+    <View style={{ flex: 1 }}>
+      {selectedTab == 0 ? (
         <Text>0</Text>
       ) : selectedTab == 1 ? (
         <Text>0</Text>
@@ -17,12 +17,12 @@ const Footer = () => {
       ) : (
         <Text>0</Text>
       )}
-     <View
+      <View
         style={{
           flexDirection: "row",
           position: "absolute",
           bottom: 0,
-          height: 60,
+          height: 64,
           backgroundColor: "#1D1F24",
         }}
       >
@@ -35,9 +35,22 @@ const Footer = () => {
           onPress={() => setSelectedTab(0)}
         >
           <Image
-            source={require("../../assets/shield.png")}
-            style={{ width: 34, height: 34, tintColor: selectedTab === 0 ? '#19A519' : '#676D75' }}
+            source={require("../../assets/track.png")}
+            style={{
+              width: 30,
+              height: 30,
+              tintColor: selectedTab === 0 ? "green" : "#676D75",
+            }}
           />
+          <Text
+            style={{
+              color: selectedTab === 0 ? "green" : "#676D75",
+              marginTop: 5,
+              fontWeight: 600
+            }}
+          >
+            Track
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={{
@@ -48,24 +61,37 @@ const Footer = () => {
           onPress={() => setSelectedTab(1)}
         >
           <Image
-            source={require("../../assets/addGroup.png")}
-            style={{ width: 30, height: 30,tintColor: selectedTab === 1 ? 'green' : '#676D75' }}
+            source={require("../../assets/shield.png")}
+            style={{
+              width: 28,
+              height: 28,
+              tintColor: selectedTab === 1 ? "#19A519" : "#676D75",
+            }}
           />
+          <Text
+            style={{
+              color: selectedTab === 1 ? "green" : "#676D75",
+              marginTop: 5,
+              fontWeight: 600
+            }}
+          >
+            Safety
+          </Text>
         </TouchableOpacity>
         <View style={{ width: "20%" }}>
           <TouchableOpacity
             style={{
               alignItems: "center",
               justifyContent: "center",
-              backgroundColor: selectedTab === 2 ? '#C7F6C7' : '#C7F6C7',
+              backgroundColor: selectedTab === 2 ? "#C7F6C7" : "#C7F6C7",
               borderRadius: 50,
               height: 70,
               width: 70,
               alignSelf: "center",
               position: "absolute",
-              bottom: 0
+              bottom: 12,
             }}
-            onPress={() => setSelectedTab(2)}
+            onPress={() => {}}
           >
             <Image
               source={require("../../assets/sos.png")}
@@ -73,24 +99,32 @@ const Footer = () => {
             />
           </TouchableOpacity>
         </View>
-        <View style={{ width: "20%" }}>
-          <TouchableOpacity
+        <TouchableOpacity
+          style={{
+            width: "20%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => setSelectedTab(3)}
+        >
+          <Image
+            source={require("../../assets/add.png")}
             style={{
-              width: 50,
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: 50,
-              height: 50,
-              alignSelf: "center",
+              width: 30,
+              height: 30,
+              tintColor: selectedTab === 3 ? "green" : "#676D75",
             }}
-            onPress={() => setSelectedTab(3)}
+          />
+          <Text
+            style={{
+              color: selectedTab === 3 ? "green" : "#676D75",
+              marginTop: 5,
+              fontWeight: 600
+            }}
           >
-            <Image
-              source={require("../../assets/add.png")}
-              style={{ width: 34, height: 34,tintColor: selectedTab === 3 ? 'green' : '#676D75' }}
-            />
-          </TouchableOpacity>
-        </View>
+            Create
+          </Text>
+        </TouchableOpacity>
         <TouchableOpacity
           style={{
             width: "20%",
@@ -101,13 +135,26 @@ const Footer = () => {
         >
           <Image
             source={require("../../assets/helpline.png")}
-            style={{ width: 34, height: 34,tintColor: selectedTab === 4 ? 'green' : '#676D75' }}
+            style={{
+              width: 28,
+              height: 28,
+              tintColor: selectedTab === 4 ? "green" : "#676D75",
+            }}
           />
-          <Text style={{color: selectedTab === 4 ? 'green' : '#676D75'}}>HelpLine</Text>
+          <Text
+            style={{
+              color: selectedTab === 4 ? "green" : "#676D75",
+              marginTop: 5,
+              fontWeight: 600,
+              marginRight: 6
+            }}
+          >
+            HelpLine
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
