09a72f786e9c3a6de3f03abe3dba3afbf0a1a4d1
anirudh-tiwari <anirudht2001@gmail.com>
2024-05-27 21:32:13 +0530

    add: dropdown component

--- pages/locality/create.js
+++ pages/locality/create.js
@@ -1,29 +1,35 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
-import { useRoute } from '@react-navigation/native';
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import { useRoute } from "@react-navigation/native";
+import Dropdown from "../../components/dropDown";
 
 const Create = () => {
   const image = useRoute();
   const { img } = image?.params;
 
-  console.log('aniimage',image)
+  console.log("aniimage", image);
 
   return (
     <View style={styles.createWrapper}>
-      <Text>Create2</Text>
-              {img && <Image source={{ uri: img }} style={{
-          width: 200,
-          height: 200,
-        }} />}
+      {img && (
+        <Image
+          source={{ uri: img }}
+          style={{
+            width: 200,
+            height: 200,
+          }}
+        />
+      )}
+      <Dropdown options={[{ label: "Anirudh"}, {label: "Avinash" }]} />
     </View>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
 
 const styles = StyleSheet.create({
-    createWrapper : {
-        flex: 1,
-        backgroundColor: "black",
-    }
-})
+  createWrapper: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+});
